12d97f23b04169a064c95ff3c43807f4bccedab2
AROHI _SHIEKH <183738874+arohisuzi@users.noreply.github.com>
2024-10-08 13:39:03 +0600

    Update prompt.js

--- scripts/cmds/prompt.js
+++ scripts/cmds/prompt.js
@@ -1,4 +1,4 @@
- const axios = require('axios');
+const axios = require('axios');
 const baseApiUrl = async () => {
   const base = await axios.get(
     `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
